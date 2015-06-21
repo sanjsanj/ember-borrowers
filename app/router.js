84a32bfe24b12c2bc9cf1fd582e91f8d491977c8
sanjsanj <sanjsanj@hotmail.com>
2015-06-21 18:09:48 +0100

    can save friends

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('friends');
+  this.resource('friends', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
