a49da690297ef9df628b4ba3153cb2f0ceedfd3f
sanjsanj <sanjsanj@hotmail.com>
2015-06-21 18:38:58 +0100

    refactor friend name li link-to

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -3,11 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     save: function(){
-      console.log('+--- save action bubbled up to friends route');
       return true;
     },
     cancel: function(){
-      console.log('+--- cancel action bubbled up to friends route');
       return true;
     }
   }
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -6,11 +6,9 @@ export default Ember.Route.extend({
   },
   actions: {
     save: function(){
-      console.log('+-- save action bubbled up to friends new route');
       return true;
     },
     cancel: function(){
-      console.log('+-- cancel action bubbled up to friends new route');
       return true;
     }
   }
