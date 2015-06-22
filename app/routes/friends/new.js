a2cdbfc6e4fb54d3e11d687c554ea2cf32f02b37
sanjsanj <sanjsanj@hotmail.com>
2015-06-22 10:29:45 +0100

    implement articles

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,14 @@ export default Ember.Route.extend({
   model: function(){
     return this.store.createRecord('friend');
   },
+  activate: function(){
+  },
+  deactivate: function(){
+    var model = this.modelFor('friends/new');
+    if (model.get('isNew')){
+      model.destroyRecord();
+    }
+  },
   actions: {
     save: function(){
       return true;
